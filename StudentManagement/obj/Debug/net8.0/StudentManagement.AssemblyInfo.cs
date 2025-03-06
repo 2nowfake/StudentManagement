0b849346ce4f1132b5a5d0e999288b6ddcec7318
2nowfake <baba_220@hotmail.com>
2025-03-06 23:33:48 +0100

    WIP: Saving my changes before pulling main

--- StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2299879da075bc918a8cb8e119818b70e74f6ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644d49f63ffbc01f20108bb98f7ffefcb416c88a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
